9b5dcad45163ed12c03f6ee8639c71189f62a463
Hogwarts23 <60142794+Hogwarts23@users.noreply.github.com>
2021-01-24 13:52:37 -0500

    Create ddns-start.sh

---
+++ ddns+nginx/ddns-start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+APIKEY="1c1a54e129b5a7d5e867"
+DOMAIN="mdaicloud.com"
+HOSTLIST=("" "www" "nas")
+IP=${1}
+
+for HOST in "${HOSTLIST[@]}"
+do
+	if [ -z "$HOST" ]; then
+	  FULLDOMAIN=$DOMAIN
+	else
+	  FULLDOMAIN=$HOST.$DOMAIN
+	fi
+
+	# Fetch DNS record ID
+	RESPONSE="$(curl -s "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIKEY&domain=$DOMAIN")"
+	RECORD_ID="$(echo $RESPONSE | sed -n "s/^.*<record_id>\(.*\)<\/record_id>.*<type>A<\/type><host>$FULLDOMAIN<\/host>.*$/\1/p")"
+
+	# Update DNS record in Namesilo
+	RESPONSE="$(curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrid=$RECORD_ID&rrhost=$HOST&rrvalue=$IP&rrttl=7207")"
+
+	# Check whether the update was successful
+	echo $RESPONSE | grep -E "<code>(280|300)</code>" &>/dev/null
+	if [ $? -eq 0 ]; then
+	  /sbin/ddns_custom_updated 1
+	else
+	  /sbin/ddns_custom_updated 0
+	fi
+done
